954fbca5e4d2730d4a3e49c6035bac528af45ebc
bhonglesrishti03-sudo <bhonglesrishti03@gmail.com>
2026-01-09 20:57:18 +0530

    This is leetcode easy

---
+++ BINARY SEARCH/ValidSquareRoot.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+
+        int low = 1, high = x;
+       
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if ((long) mid * mid == x) {
+                return true;
+            }
+
+            if ((long) mid * mid < x) {
+               
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
