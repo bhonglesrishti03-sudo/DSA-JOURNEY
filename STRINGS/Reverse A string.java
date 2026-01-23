aa1e67a1903de41e99ea9629181cdc224c314308
bhonglesrishti03-sudo <bhonglesrishti03@gmail.com>
2026-01-23 21:21:50 +0530

    This is the way of using two pointers in java

--- STRINGS/Reverse A string.java
+++ STRINGS/Reverse A string.java
@@ -9,12 +9,25 @@ class solution{
     //    System.out.println(ans);
 
      
-       String s = "SRISHTI";
-       StringBuilder sb = new StringBuilder();
-       for(int i=s.length()-1; i>=0;i--){
-           sb.append(s.charAt(i));
+    //    String s = "SRISHTI";
+    //    StringBuilder sb = new StringBuilder();
+    //    for(int i=s.length()-1; i>=0;i--){
+    //        sb.append(s.charAt(i));
+    //    }
+    //    System.out.println(sb.toString());
+
+    String s = "hello";
+       char[]arr = s.toCharArray();
+       int i=0;
+       int j = arr.length-1;
+       while(i<j){
+           char temp = arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           i++;
+           j--;
        }
-       System.out.println(sb.toString());
+       System.out.println(new String(arr));
     }
 
 
