3585e1ae5f4d75822760f55457c086eb52017345
bhonglesrishti03-sudo <bhonglesrishti03@gmail.com>
2026-01-23 21:20:48 +0530

    This is the way of using StringBuilders in java

--- STRINGS/Reverse A string.java
+++ STRINGS/Reverse A string.java
@@ -1,11 +1,21 @@
 class solution{
     public static void main(String[]args){
-        String s = "SRISHTI";
-       //manual reverse
-       String ans = "";
-       for(int i=s.length()-1; i>=0; i--){
-           ans = ans + s.charAt(i);
+    //     String s = "SRISHTI";
+    //    //manual reverse
+    //    String ans = "";
+    //    for(int i=s.length()-1; i>=0; i--){
+    //        ans = ans + s.charAt(i);
+    //    }
+    //    System.out.println(ans);
+
+     
+       String s = "SRISHTI";
+       StringBuilder sb = new StringBuilder();
+       for(int i=s.length()-1; i>=0;i--){
+           sb.append(s.charAt(i));
        }
-       System.out.println(ans);
+       System.out.println(sb.toString());
     }
+
+
 }
