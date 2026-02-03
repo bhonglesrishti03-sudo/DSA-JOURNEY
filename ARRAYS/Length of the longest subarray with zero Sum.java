53023e56dd90e16a3a874067b27d7631770b86df
bhonglesrishti03-sudo <bhonglesrishti03@gmail.com>
2026-02-03 21:14:08 +0530

    Optimized Version

--- ARRAYS/Length of the longest subarray with zero Sum.java
+++ ARRAYS/Length of the longest subarray with zero Sum.java
@@ -26,22 +26,48 @@ The length of the longest subarray with sum zero is 8.
 */
 
 //brute force approach
+// class Solution{
+//     public static void main(String[]args){
+//         int[] array = {6, -2, 2, -8, 1, 7, 4, -10};
+//         System.out.print(longestArraySum(array));
+//     }
+//     static int longestArraySum(int[]array){
+//         int maxLen=0;
+//         for(int i=0; i<array.length; i++){
+//             int sum=0;
+//             for(int j=i+1; j<array.length;j++){
+//                 sum+=array[j];
+//                 if(sum==0){
+//                     maxLen = Math.max(maxLen,j-i+1);
+//                 }
+//             }
+//         }
+//         return maxLen;
+//     }
+// }
+
+// better approach
+//we can use a hashmap to check prefix sum
 class Solution{
     public static void main(String[]args){
         int[] array = {6, -2, 2, -8, 1, 7, 4, -10};
         System.out.print(longestArraySum(array));
     }
     static int longestArraySum(int[]array){
-        int maxLen=0;
-        for(int i=0; i<array.length; i++){
-            int sum=0;
-            for(int j=i+1; j<array.length;j++){
-                sum+=array[j];
-                if(sum==0){
-                    maxLen = Math.max(maxLen,j-i+1);
-                }
-            }
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int maxLen =0;
+        int sum=0;
+        for(int i=0; i<array.length;i++){
+        sum+=array[i];
+        if(sum == 0){
+            maxLen = i+1;
         }
-        return maxLen;
+        if(map.contains(sum)){
+            maxLen = Math.max(maxLen, i - map.get(sum));
+        }else{
+            map.put(sum,i);
+        }
     }
+    return maxLen;
 }
