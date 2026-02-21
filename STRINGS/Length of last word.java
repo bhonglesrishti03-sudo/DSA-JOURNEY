3c2dce906be07a0956572a4ef7e6c3238196093b
bhonglesrishti03-sudo <bhonglesrishti03@gmail.com>
2026-02-21 22:04:05 +0530

    LEETCODE EASY

---
+++ STRINGS/Length of last word.java
@@ -0,0 +1,15 @@
+int i = s.length()-1;
+int count = 0;
+
+    // skip trailing spaces
+    while (i >= 0 && s.charAt(i) == ' ') {
+        i--;
+    }
+
+    // count last word
+    while (i >= 0 && s.charAt(i) != ' ') {
+        count++;
+        i--;
+    }
+
+    return count;
